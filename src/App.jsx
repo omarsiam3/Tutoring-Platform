edc5ba2e90d3cca8671ca423880347d83f996a36
privilledge <privieroyalty@gmail.com>
2024-01-22 13:34:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import "bootstrap/dist/css/bootstrap.css";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
